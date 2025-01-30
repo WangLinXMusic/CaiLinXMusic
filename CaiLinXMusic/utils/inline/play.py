5cab4393c434174c6b47adc64e20b45437d715ef
WangLinXMusic <pyaesone5official@gmail.com>
2025-01-30 08:23:44 +0630

    Update play.py

--- CaiLinXMusic/utils/inline/play.py
+++ CaiLinXMusic/utils/inline/play.py
@@ -71,7 +71,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
                 text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/Iso_Lost_Control"
             ),
             InlineKeyboardButton(
-                text=" ᴄʜᴀᴛ ", url=f"https://t.me/seriousvs_version10"
+                text=" ᴄʜᴀᴛ ", url=f"https://t.me/seriousvs_version20"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -93,7 +93,7 @@ def stream_markup(_, videoid, chat_id):
                 text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/Iso_Lost_Control"
             ),
             InlineKeyboardButton(
-                text=" ᴄʜᴀᴛ ", url=f"https://t.me/seriousvs_version10"
+                text=" ᴄʜᴀᴛ ", url=f"https://t.me/seriousvs_version20"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
